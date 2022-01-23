7b415f8a78d015dfe9c62f6bb88bf9b079ad46e6
CEED87 <ceed210787@gmail.com>
2022-01-23 17:34:48 +0300

    interface

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="counter">0</div>
+    <div className="controls">
+      <button>INC</button>
+      <button>DEC</button>
+      <button>RND</button>
+      <button>RESET</button>
+    </div>
+  </div>
+  );
+}
+
+export default App;
